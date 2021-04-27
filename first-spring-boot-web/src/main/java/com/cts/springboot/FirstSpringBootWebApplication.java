e0ac7e8c6afe904f77cb3ef8cd4d2ec7ec1dedc9
sai <saimachavarapu772@gmail.com>
2021-04-27 11:49:34 +0530

    first commit

---
+++ first-spring-boot-web/src/main/java/com/cts/springboot/FirstSpringBootWebApplication.java
@@ -0,0 +1,13 @@
+package com.cts.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringBootWebApplication.class, args);
+	}
+
+}
